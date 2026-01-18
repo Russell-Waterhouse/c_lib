0a6e49387b73b8bd37d135748b751bd035cd3f48
Russell-Waterhouse <Russell.L.Waterhouse@gmail.com>
2026-01-17 23:52:37 -0700

    fixing imports

--- templates/dynamic_array.h
+++ templates/dynamic_array.h
@@ -22,6 +22,8 @@ void GENERIC_TYPE_free(GENERIC_TYPEDynArr a);
 GENERIC_TYPEDynArr GENERIC_TYPE_dyn_arr_initialize(size_t size);
 u8 GENERIC_TYPE_equal(GENERIC_TYPEDynArr a1, GENERIC_TYPEDynArr a2);
 
+#endif 
+
 #ifdef GENERIC_TYPE_DYNAMIC_ARRAY_IMPLEMENTATION
 GENERIC_TYPEDynArr GENERIC_TYPE_dyn_arr_initialize(size_t size) {
   GENERIC_TYPEDynArr a;
@@ -93,5 +95,4 @@ u8 GENERIC_TYPE_equal(GENERIC_TYPEDynArr a1, GENERIC_TYPEDynArr a2) {
   return 1;
 }
 
-#endif
 #endif

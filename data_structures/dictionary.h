50c9c022d7d6ce8f6eba16dd29dfa5015ca3c7c6
Russell Waterhouse <Russell.L.Waterhouse@gmail.com>
2026-02-12 22:42:28 -0700

    Update dictionary.h

--- data_structures/dictionary.h
+++ data_structures/dictionary.h
@@ -3,14 +3,20 @@
 #include "../types/strings.h"
 #include "../types/types.h"
 
-typedef struct DictNode{
+typedef struct DictNode {
   String key;
   String val;
   struct DictNode* next;
 } Dictionary;
 
-Dictionary* dict_upsert(Arena* arena, Dictionary* dict, String key, String val);
+typedef struct  {
+   Result res;
+   Dictionary* dict:
+   Error err;
+} DictionaryResult;
+
+DictionaryResult* dict_upsert(Arena* arena, Dictionary* dict, String key, String val);
 StrResult dict_get(Dictionary* dict, String key);
-IOResult dict_delete(Dictionary* dict, String key);
+DictionaryResult dict_delete(Dictionary* dict, String key);
 
 #endif

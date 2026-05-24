3525d7681d8792061766d074d12d6bd4489784fc
Russell-Waterhouse <Russell.L.Waterhouse@gmail.com>
2026-05-23 18:25:00 -0600

    add exports

--- export/unity_build.c
+++ export/unity_build.c
@@ -3,4 +3,5 @@
 #include "../data_structures/i64_dynamic_array.c"
 #include "../data_structures/size_t_dynamic_array.c"
 #include "../core/casts.c"
-
+#include "../types/strings.c"
+#include "../memory/arenas.c"
